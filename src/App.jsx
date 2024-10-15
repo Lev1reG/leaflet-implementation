18027c17c25a597ca5449d4edfd6d75514675a7e
Lev1reG <derentanaphan@mail.ugm.ac.id>
2024-10-15 17:29:26 +0700

    feat: add leaflet map

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import './styles.css'
+import './styles.css';
+import "leaflet/dist/leaflet.css";
+
+import {
+  MapContainer,
+  TileLayer
+} from 'react-leaflet';
 
 function App() {
+  const position = [-7.773782879038681, 110.37836455755829];
+
   return (
     <>
-      <div className="App">
-        Hello
-      </div>
+      <MapContainer
+        center={position}
+        zoom={17}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://gamaforce.wg.ugm.ac.id/">GAMAFORCE UGM</a>'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
     </>
   )
 }
