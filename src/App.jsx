8524cb1c78ca0d3d4b6a435b6f8a8f12645f0dc8
Lev1reG <derentanaphan@mail.ugm.ac.id>
2024-10-15 22:33:00 +0700

    feat: add custom icon for markers

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
+import { Icon } from 'leaflet';
 import './styles.css';
 import "leaflet/dist/leaflet.css";
 
 import {
   MapContainer,
-  TileLayer
+  TileLayer,
+  Marker
 } from 'react-leaflet';
 
 function App() {
   const position = [-7.773782879038681, 110.37836455755829];
+
+  const markers = [
+    {
+      geocode: [-7.773782879038681, 110.37836455755829],
+      popup: 'Gamaforce UGM'
+    },
+    {
+      geocode: [-7.770113921502923, 110.37789475890168],
+      popup: 'Grha Sabha Prmana (GSP) UGM'
+    }
+  ]
+
+  const customIcon = new Icon({
+    iconUrl: '/pin.png',
+    iconSize: [38, 38]
+  });
 
   return (
     <>
@@ -19,6 +37,17 @@ function App() {
           attribution='&copy; <a href="https://gamaforce.wg.ugm.ac.id/">GAMAFORCE UGM</a>'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
+
+        {
+          markers.map((marker) => (
+            <Marker
+              position={marker.geocode}
+              icon={customIcon}
+            >
+
+            </Marker>
+          ))
+        }
       </MapContainer>
     </>
   )
